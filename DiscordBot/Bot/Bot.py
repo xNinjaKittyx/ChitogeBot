80f377bfd2d0641e6ce5c78265eafcd8ccbe35b5
Daniel Ahn <sangbumahn@gmail.com>
2015-11-13 18:32:54 -0800

    Attempt to fix exec/eval/debug

--- DiscordBot/Bot/Bot.py
+++ DiscordBot/Bot/Bot.py
@@ -58,46 +58,31 @@ def cinfo(message):
 def debug(message):
     argname = message.content[7:]
 
-    def worker():
-        if message.author.id == "82221891191844864":
-            try:
-                client.send_message(message.channel, "```{}```".format(exec(argname)))
-            except SyntaxError as err:
-                client.send_message(message.channel, "```{}```".format(err))
-
-    t = threading.Thread(target=worker)
-    t.daemon = True
-    t.start()
+    if message.author.id == "82221891191844864":
+        try:
+            client.send_message(message.channel, "```{}```".format(eval(argname)))
+        except SyntaxError as err:
+            client.send_message(message.channel, "```{}```".format(err))
 
 
 def exec(message):
     argname = message.content[6:]
 
-    def worker():
-        if message.author.id == "82221891191844864":
-            try:
-                client.send_message(message.channel, "```{}```".format(exec(argname)))
-            except SyntaxError as err:
-                client.send_message(message.channel, "```{}```".format(err))
-
-    t = threading.Thread(target=worker)
-    t.daemon = True
-    t.start()
+    if message.author.id == "82221891191844864":
+        try:
+            client.send_message(message.channel, "```{}```".format(exec(argname)))
+        except SyntaxError as err:
+            client.send_message(message.channel, "```{}```".format(err))
 
 
 def eval(message):
     argname = message.content[6:]
 
-    def worker():
-        if message.author.id == "82221891191844864":
-            try:
-                client.send_message(message.channel, "```{}```".format(eval(argname)))
-            except SyntaxError as err:
-                client.send_message(message.channel, "```{}```".format(err))
-
-    t = threading.Thread(target=worker)
-    t.daemon = True
-    t.start()
+    if message.author.id == "82221891191844864":
+        try:
+            client.send_message(message.channel, "```{}```".format(eval(argname)))
+        except SyntaxError as err:
+            client.send_message(message.channel, "```{}```".format(err))
 
 
 def hello(message):
