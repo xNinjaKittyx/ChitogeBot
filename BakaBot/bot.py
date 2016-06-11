d61c46bd4e1162a31e9fde2e8d9220282ef1c472
Daniel Ahn <xNinjaKittyx@users.noreply.github.com>
2016-06-11 14:06:36 -0700

    Update bot.py

--- BakaBot/bot.py
+++ BakaBot/bot.py
@@ -128,7 +128,7 @@ if __name__ == "__main__":
     bot.load_extension('modules.musicplayer')
     bot.load_extension('modules.mal')
     bot.load_extension('modules.pad')
-    bot.run('MTg5OTM5NTI0NjY1NzM3MjE3.Cjke-Q.Kj4uv-WyUeNCxtZ7yokSb66TxhE')
+    bot.run('')
     bot.run('*')
 # def checkPrivate(message):
 #     """Checks if the message is a PM"""
