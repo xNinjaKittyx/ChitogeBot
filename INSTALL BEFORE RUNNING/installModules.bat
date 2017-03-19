d281c6f975f41eb9d30b3836540ecdd78dd07df2
Daniel Ahn <sangbumahn@gmail.com>
2017-03-19 04:03:39 -0700

    Changing Cleverbot to Cleverwrap
    only the batch scripts.

--- INSTALL BEFORE RUNNING/installModules.bat
+++ INSTALL BEFORE RUNNING/installModules.bat
@@ -4,7 +4,7 @@ pip install wikipedia --upgrade
 pip install youtube-dl --upgrade
 pip install requests --upgrade
 pip install xmltodict --upgrade
-pip install cleverbot --upgrade
+pip install cleverwrap --upgrade
 pip install Pillow --upgrade
 pip install beautifulsoup4 --upgrade
 pause
--- INSTALL BEFORE RUNNING/installModules.sh
+++ INSTALL BEFORE RUNNING/installModules.sh
@@ -4,7 +4,7 @@ pip3 install wikipedia --upgrade
 pip3 install youtube-dl --upgrade
 pip3 install requests --upgrade
 pip3 install xmltodict --upgrade
-pip3 install cleverbot --upgrade
+pip3 install cleverwrap --upgrade
 pip3 install Pillow --upgrade
 pip install beautifulsoup4 --upgrade
 sudo apt-get install ffmpeg
